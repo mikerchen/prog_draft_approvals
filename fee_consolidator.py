478ce8f2bd3c9542016ceef174563d330cbdbcb8
Michael Chen <chen.michaelr@gmail.com>
2021-08-25 00:58:52 -0700

    8.25.21 update

---
+++ fee_consolidator.py
@@ -0,0 +1,11 @@
+from typing import Match
+
+
+clinch, DCM, Invoice report
+
+Match
+Ingest invoice file, map reconciled status
+calculate DCM
+plot against keys
+
+Output: Summed Sheet
--- prog_approvals.py
+++ prog_approvals.py
@@ -3,28 +3,45 @@ from datetime import datetime
 
 #Import Approvals and Prisma Workbook
 approvals = pd.read_excel('Input/Approvals Grid.xlsx', sheet_name = 0)
-prisma = pd.read_excel('Input/Prisma.xlsx', sheet_name = 0)
+prisma = pd.read_excel('Input/Prisma.xlsx', header=1, sheet_name = 0)
+# fees_sums = pd.read_excel('summed sheet')
 
-prisma = prisma.rename(columns={'Month of service': 'Month of Service', 'Estimate code':'Est', 'Supplier name': 'Publisher','Billed':'Ordered'})
+print('Rows found in Draft Approvals Sheet: ' + str(approvals['Est'].count()))
+print('Rows found in Prisma Sheet: ' + str(prisma['Estimate code'].count()))
+
+prisma = prisma.rename(columns={'Month of service': 'Month of Service', 'Estimate code':'Est', 'Supplier short name': 'Publisher','Billed':'Ordered'})
 
 #Convert Month of Service to datetime
 approvals['Month of Service'] = pd.to_datetime(approvals['Month of Service'])
 prisma['Month of Service'] = pd.to_datetime(prisma['Month of Service'])
 
-# #Concatenate Estimate, MoS, Supplier
-# approvals['UI'] = approvals['Est'].map(str) + approvals['Month of Service'].map(str) + approvals['Publisher']
-# prisma['UI'] = prisma['Est'].map(str) + prisma['Month of Service'].map(str) + prisma['Publisher']
-
 match = approvals.merge(prisma, how='inner',left_on=['Est','Month of Service','Publisher'],right_on=['Est','Month of Service','Publisher'],indicator=True)
 
 match['Approval Status'] = ''
+match['Estimate Status'] = ''
 
 for i, j in match.iterrows():
     if(match['Product Name'][i] != 'Fees'):
         if(match['Ordered'][i] == match['Actual Net Billable'][i]):
             match.at[i,'Approval Status'] = 'Media Buy: Approved'
         else:
-            match.at[i,'Approval Status'] = 'Media Buy: Ordered <> Billable'
+            diff = match['Ordered'][i] - match['Actual Net Billable'][i]
+            diff_cur = "${:,.2f}".format(diff)
+            match.at[i,'Approval Status'] = 'Media Buy (Ordered <> Billable): Delta = ' + str(diff_cur)
     else:
         match.at[i, 'Approval Status'] = 'Fee Buy: Unknown'
+
+for i, j in match.iterrows():
+    if('Media Buy: Ordered <> Billable' in match['Approval Status'][i]):
+        estimate = match['Est'][i]
+        for k, w in match.iterrows():
+            if(match['Est'][k] == estimate):
+                match.at[k,'Estimate Status'] = 'Dependent'
+
+# fees_sums
+
+# Estimate Key against a summed sheet
+# 	If fee is reconciled or fee does not have a previous month's billing, check Ordered vs Billed
+# 	If fee isn't reconciled, then fee must be within $300 +/-, previous month OR a delta (Calculated Fee - previous month's ordered)
+
 match.to_csv('test.csv')
