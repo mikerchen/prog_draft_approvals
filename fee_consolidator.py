e6fd2f27be5978f643c624e452f0cb7d7363c63c
Michael Chen <chen.michaelr@gmail.com>
2021-08-25 23:43:30 -0700

    8.25 update

--- fee_consolidator.py
+++ fee_consolidator.py
@@ -1,11 +1,52 @@
-from typing import Match
+from numpy import int64
+import pandas as pd
+from datetime import date
+
+now = date.today().strftime('%B_%d_%Y')
+
+dcm_rates = {
+    'Display': 0.035,
+    'Tracking': 0.035,
+    'In-Stream Audio': 0.250,
+    'In-Stream Video': 0.250,
+    'Rich Media display banner': 0.110
+}
+
+dcm = pd.read_excel('Input/DCM.xlsx', sheet_name = 0)
+prisma = pd.read_excel('Input/Prisma.xlsx', header=1, sheet_name = 0, usecols=['Campaign name','Estimate code', 'Month of service', 'Supplier short name'])
+
+dcm['Month'] = pd.to_datetime(dcm['Month'])
+dcm['Publisher'] = 'DCM'
+dcm['Cost'] = ''
+
+prisma = prisma.rename(columns={'Month of service': 'Month', 'Supplier short name': 'Publisher', 'Campaign name': 'Campaign'})
+prisma['Month'] = pd.to_datetime(prisma['Month'])
+
+for i, j in dcm.iterrows():
+    if dcm['Creative Type'][i] in dcm_rates.keys():
+        fee = (dcm['Impressions'][i]/1000)*dcm_rates[dcm['Creative Type'][i]]
+        fee_cur = "{:.2f}".format(fee)
+        dcm.at[i,'Cost'] = fee_cur
+    else:
+        dcm.at[i,'Cost'] = 'Invalid Creative Type: ' + str(dcm['Creative Type'][i])
+
+dcm_joined = dcm.merge(prisma, how='left', on=['Month', 'Publisher', 'Campaign'] )
+
+dcm_joined.to_csv('Output/DCM_Fees_' + now + '.csv')
+
+dcm_joined['Cost'] = pd.to_numeric(dcm_joined['Cost'])
+
+dcm_grouped = dcm_joined.groupby(['Estimate code','Month','Publisher'])['Cost'].sum()
+dcm_grouped.to_csv('Output/DCM_Grouped_' + now + '.csv')
 
 
-clinch, DCM, Invoice report
+print('Process Successful')
 
-Match
-Ingest invoice file, map reconciled status
-calculate DCM
-plot against keys
+# clinch, DCM, Invoice report
 
-Output: Summed Sheet
+# Match
+# Ingest invoice file, map reconciled status
+# calculate DCM
+# plot against keys
+
+# Output: Summed Sheet
--- prog_approvals.py
+++ prog_approvals.py
@@ -1,24 +1,31 @@
 import pandas as pd
-from datetime import datetime
+from datetime import date
+import time
+from progress_bar import printProgressBar
+
+now = date.today().strftime('%B_%d_%Y')
+
+prisma_cols = ['Month of service', 'Estimate code', 'Supplier short name', 'Actual Net Billable', 'Placement ID', 'Placement name']
 
 #Import Approvals and Prisma Workbook
 approvals = pd.read_excel('Input/Approvals Grid.xlsx', sheet_name = 0)
-prisma = pd.read_excel('Input/Prisma.xlsx', header=1, sheet_name = 0)
+prisma = pd.read_excel('Input/Prisma.xlsx', header=1, sheet_name = 0, usecols=prisma_cols)
 # fees_sums = pd.read_excel('summed sheet')
 
-print('Rows found in Draft Approvals Sheet: ' + str(approvals['Est'].count()))
-print('Rows found in Prisma Sheet: ' + str(prisma['Estimate code'].count()))
-
-prisma = prisma.rename(columns={'Month of service': 'Month of Service', 'Estimate code':'Est', 'Supplier short name': 'Publisher','Billed':'Ordered'})
+prisma = prisma.rename(columns={'Month of service': 'Month of Service', 'Estimate code':'Est', 'Supplier short name': 'Publisher'})
 
 #Convert Month of Service to datetime
 approvals['Month of Service'] = pd.to_datetime(approvals['Month of Service'])
 prisma['Month of Service'] = pd.to_datetime(prisma['Month of Service'])
 
-match = approvals.merge(prisma, how='inner',left_on=['Est','Month of Service','Publisher'],right_on=['Est','Month of Service','Publisher'],indicator=True)
+match = approvals.merge(prisma, how='left',on=['Est','Month of Service','Publisher'])
 
 match['Approval Status'] = ''
 match['Estimate Status'] = ''
+
+print('Rows found in Draft Approvals Sheet: ' + str(approvals['Est'].count()))
+print('Rows found in Prisma Sheet: ' + str(prisma['Est'].count()))
+print('Rows found in Match Sheet: ' + str(match['Est'].count()))
 
 for i, j in match.iterrows():
     if(match['Product Name'][i] != 'Fees'):
@@ -31,17 +38,29 @@ for i, j in match.iterrows():
     else:
         match.at[i, 'Approval Status'] = 'Fee Buy: Unknown'
 
+items = list(range(0,match['Est'].count()))
+l = len(items)
+
+# printProgressBar(0, l, prefix = 'Progress:', suffix = '', length = 50)
+
 for i, j in match.iterrows():
-    if('Media Buy: Ordered <> Billable' in match['Approval Status'][i]):
+    # for t, item in enumerate(items):
+    if('Media Buy (Ordered <> Billable)' in match['Approval Status'][i]):
         estimate = match['Est'][i]
         for k, w in match.iterrows():
             if(match['Est'][k] == estimate):
                 match.at[k,'Estimate Status'] = 'Dependent'
-
+    print(str(i + 1) + '/' + str(match['Est'].count()) + ' Complete')
+        # time.sleep(0.1)
+        # printProgressBar(t + 1, l, prefix = 'Progress:', suffix = str(t + 1) + '/' + str(match['Est'].count()), length = 50)
+       
 # fees_sums
 
 # Estimate Key against a summed sheet
 # 	If fee is reconciled or fee does not have a previous month's billing, check Ordered vs Billed
 # 	If fee isn't reconciled, then fee must be within $300 +/-, previous month OR a delta (Calculated Fee - previous month's ordered)
 
-match.to_csv('test.csv')
+
+draft_approvals_output = 'draft_approvals_output_' + now + '.csv'
+
+match.to_csv('Output/' + draft_approvals_output)
